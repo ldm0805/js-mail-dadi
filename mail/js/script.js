eee2cbeed2640a8757f63a26d378741edc0e3ec1
ldm0805 <leonardodimaggio94@gmail.com>
2022-11-30 15:29:51 +0100

    Esercizio mail completo

---
+++ mail/js/script.js
@@ -0,0 +1,27 @@
+// 1. Creare un'array lista con delle email che posso accedere;
+
+const mail_list =['pippo@gmail.com', 'pluto@gmail.com', 'paperino@gmail.com', 'minnie@gmail.com'];
+
+// 2. Chiedere all'utente di inserire la sua mail;
+
+let mail = prompt('Inserisci la tua email');
+
+//Effettuare il controllo tramite la variabile booleana
+
+let bool = false;
+
+for(let i=0; i<mail_list.length; i++){
+
+    if (mail_list[i] == mail){
+        bool = true;   
+    }
+    }
+    //3.1 SE la mail risulta nella lista, ALLORA il messaggio sarà "Complimenti hai effettuato l'accesso!"
+        if (bool){
+            console.log(`Complimenti hai effettuato l'accesso!`)
+        }
+
+    //3.2 SE la mail non risulta nella lista, ALLORA il messaggio sarà "Questa email non è presente nella lista"
+        else{
+            console.log(`Questa email non è presente nella lista`)
+        }
--- mail/readme.txt
+++ mail/readme.txt
@@ -1,8 +1,8 @@
 Mail
 Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.  Non utilizzare le funzioni indexOf o includes!
 
-1. Creare una lista con delle email che posso accedere;
-2. Chiedere all'utente di inserire las sua mail;
-3. Stampare un messaggio:
+1. Creare un'array lista con delle email che posso accedere;
+2. Chiedere all'utente di inserire la sua mail;
+3. Effettuare il controllo, e stampare l'apposito messaggio secondo il risultato:
     3.1 SE la mail risulta nella lista, ALLORA il messaggio sarà "Complimenti hai effettuato l'accesso!"
-    3.2 SE la mail non risulta nella lista, ALLORA il messaggio sarà "Questa email non è presente nella lista "
+    3.2 SE la mail non risulta nella lista, ALLORA il messaggio sarà "Questa email non è presente nella lista"
