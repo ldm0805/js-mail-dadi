4de66c48cc3694121d7b34c8e636adebf2be8358
ldm0805 <leonardodimaggio94@gmail.com>
2022-12-03 00:03:39 +0100

    Ultime modifiche

--- dadi/bonus/js/script.js
+++ dadi/bonus/js/script.js
@@ -1,53 +1,26 @@
 // // Gioco dei dadi Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
-// // 1. Generare un numero casuale per il giocatore;
-// let n_player = Math.floor( Math.random() * 6) + 1;
-// // 2. Generare un numero casuale per il computer;
-// let n_computer = Math.floor( Math.random() * 6) + 1;
+//Dichiarazioni variabili
 
-// console.log(`Il numero del giocatore è ${n_player}`);
-// console.log(`Il numero del computer è ${n_computer}`);
+let n_player, n_player_2, button
+let flag = 0, game = 0, game_two = 0, game_three = 0 , counter = 0; 
 
-// // 3. Controllare le seguenti condizioni:
-
-// //     3.1 SE il giocatore ed il computer hanno lo stesso punteggio , mostrare il messaggio "è pareggio";
-// if (n_player == n_computer){
-//     console.log('Pareggio');
-// }
-
-// //     3.2 SE il computer ha il dado con il punteggio più alto , mostrare il messaggio "Il computer ha vinto";
-// else if (n_computer > n_player){
-//     console.log('Il computer ha vinto');
-// }
-
-// //     3.3 SE le precedenti condizioni non risultano vere, il giocatore ha il dado con il punteggio più alto , mostrare il messaggio "Il giocatore ha vinto";
-// else{
-//     console.log('Il giocatore ha vinto');
-// }
-
-
-let n_player
-let n_player_2
-
-
-let flag = 0;
-let game = 0;
-let game_two = 0;
-let game_three = 0;
-let counter = 0;
-let button = document.getElementById("dicebutton");
+//Creazione bottone ed azione bottone
+button = document.getElementById("dicebutton");
 button.addEventListener("click", rollDice);
 function rollDice() {
-  
-  if(counter < 3){
 
-    n_player = Math.floor( Math.random() * 6) + 1;
-    document.getElementById('numero').innerHTML = `Il numero del giocatore è ${n_player}.`;
-    
-    n_player_2 = Math.floor( Math.random() * 6) + 1;
-    document.getElementById('numero_2').innerHTML = `Il numero del pc è ${n_player_2}.`;
+    //Generare 3 numeri random per entrambi i giocatori, tre volte
+      if(counter < 3){
 
-    if(n_player > n_player_2){
+    // Creazione numeri randomici per i due player
+      n_player = Math.floor( Math.random() * 6) + 1;
+      document.getElementById('numero').innerHTML = `Il numero del giocatore è ${n_player}.`;
+      
+      n_player_2 = Math.floor( Math.random() * 6) + 1;
+      document.getElementById('numero_2').innerHTML = `Il numero del pc è ${n_player_2}.`;
+
+      if(n_player > n_player_2){
         game++
       }
 
@@ -62,7 +35,8 @@ function rollDice() {
       /* Add a class of "roll" to #dice after it's clicked, so that the transform properties come to life: */
       dice.classList.toggle("roll");
     }
-
+    
+    //Calcolo risultati
     if(counter == 3){
 
      if(game == game_two){
