a10fa4656e153c03617d9d6b46550dae539b16c1
ldm0805 <leonardodimaggio94@gmail.com>
2022-12-03 09:49:23 +0100

    Inseriti contatori del punteggio, inserimento del numero di tiri da prompt

--- dadi/bonus/js/script.js
+++ dadi/bonus/js/script.js
@@ -4,40 +4,54 @@
 
 let n_player, n_player_2, button
 let flag = 0, game = 0, game_two = 0, game_three = 0 , counter = 0; 
-
+let shoot = parseInt(prompt('Inserisci il numero di tiri che vuoi effettuare'));
 //Creazione bottone ed azione bottone
 button = document.getElementById("dicebutton");
 button.addEventListener("click", rollDice);
 function rollDice() {
+  
+  //Generare 3 numeri random per entrambi i giocatori, tre volte
+  if(counter < shoot){
 
-    //Generare 3 numeri random per entrambi i giocatori, tre volte
-      if(counter < 3){
 
     // Creazione numeri randomici per i due player
       n_player = Math.floor( Math.random() * 6) + 1;
-      document.getElementById('numero').innerHTML = `Il numero del giocatore è ${n_player}.`;
+      document.getElementById('numero').innerHTML = `Il numero del giocatore umano è ${n_player}.`;
       
       n_player_2 = Math.floor( Math.random() * 6) + 1;
-      document.getElementById('numero_2').innerHTML = `Il numero del pc è ${n_player_2}.`;
+      document.getElementById('numero_2').innerHTML = `Il numero del giocatore virtuale è ${n_player_2}.`;
 
       if(n_player > n_player_2){
         game++
+        document.getElementById('result_part').innerHTML =``;
+        console.log(game)
       }
 
       else if(n_player < n_player_2){
+        
         game_two++ 
+        document.getElementById('result_part').innerHTML =` game`;
       }
-      
       else{
         game_three++ 
+       
       }
+      document.getElementById('result_part').innerHTML =
+      `Giocatore umano ${game}. 
+      <br> 
+      Giocatore virtuale ${game_two}. 
+      <br> 
+      Pareggi ${game_three}.`;
       counter++
       /* Add a class of "roll" to #dice after it's clicked, so that the transform properties come to life: */
       dice.classList.toggle("roll");
-    }
+
+  }
     
+
+
     //Calcolo risultati
-    if(counter == 3){
+    if(counter == shoot){
 
      if(game == game_two){
        document.getElementById('result_game').innerHTML ='Pareggio.';
@@ -49,6 +63,6 @@ function rollDice() {
 
      else{
       document.getElementById('result_game').innerHTML ='Il giocatore umano ha vinto.';
-     }
     }
   }
+}
